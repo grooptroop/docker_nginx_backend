e236e6d104b3998bc3168b658c58ff95510f973b
grooptop <pogulay.vsevolod@gmail.com>
2026-02-05 18:23:13 +0300

    v 0.1 add backend + dockerignore + gitignore

---
+++ backend/app.py
@@ -0,0 +1,15 @@
+from http.server import BaseHTTPRequestHandler, HTTPServer
+
+
+class Handler(BaseHTTPRequestHandler):
+    def do_GET(self):
+        if self.path == '/':
+            self.send_response(200)
+            self.send_header('Content-type', 'text/plain')  
+            self.end_headers()
+            self.wfile.write(b'Hello from Effective Mobile!')  
+
+
+if __name__ == '__main__':
+    server = HTTPServer(('0.0.0.0', 8080), Handler)
+    server.serve_forever()
